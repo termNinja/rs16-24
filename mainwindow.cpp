5398351689b31cd1562eb60f7b00152116eeca42
lazar <lazar.rankovic.94@gmail.com>
2016-05-15 20:29:25 +0200

    Test pull

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <QTableView>
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QLayout>
+#include <QLayoutItem>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +17,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_10_clicked()
+{
+
+    QVBoxLayout *vbox = new QVBoxLayout;
+    QPushButton *button1 = new QPushButton("One");
+    vbox->addWidget(button1);
+    button1->setGeometry(QRect(QPoint(100, 100),QSize(200, 300)));
+    ui->groupBox->setLayout(vbox);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_10_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
