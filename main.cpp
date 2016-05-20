ff7bc1bfbfa6ed1f6b5680d12cc8a9de70255d8e
termninja <nmicovic@outlook.com>
2016-05-20 19:30:35 +0200

    Removed codegen testing from main.

--- main.cpp
+++ main.cpp
@@ -6,28 +6,15 @@
 #include "moduleCodegen/type.hpp"
 #include "moduleCodegen/variable.hpp"
 #include "moduleCodegen/function.hpp"
+#include "moduleCodegen/codegentester.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
-	// TESTING CODE
-
-	// Type test
-    //Type type(QString("double"), true);
-//	std::cout << type << std::endl;
-
-	// Variable test
-    //Variable var(type, QString("x"));
-    //std::cout << var << std::endl;
-
-	// Function test
-    //std::vector<Variable> params;
-    //params.push_back(var);
-    //params.push_back(Variable(Type(QString("QString")), QString("y")));
-    //Function fun(type, QString("calculate"), params);
-    //std::cout << fun << std::endl;
+	codegen::codegenTester tester;
+	tester.peformTest();
 
 	w.show();
 	return a.exec();
