cd76c054d247d5b7bf5b245692ef1b9013e71dda
termninja <nmicovic@outlook.com>
2016-05-22 17:48:18 +0200

    Extended function into memberFunction

--- moduleCodegen/memberfunction.cpp
+++ moduleCodegen/memberfunction.cpp
@@ -1,6 +1,27 @@
 #include "memberfunction.hpp"
 
-MemberFunction::MemberFunction()
+namespace codegen {
+
+MemberFunction::MemberFunction(codegen::Type returnType, QString functionName,
+							   const std::vector<codegen::Variable> &parameters, QString owner)
+	: Function(returnType, functionName, parameters), m_owner(owner)
 {
 
 }
+
+QString MemberFunction::getOwnerName() const
+{
+	return m_owner;
+}
+
+std::ostream &operator<<(std::ostream &out, const MemberFunction &function)
+{
+	out << function.m_returnType << " " << function.m_owner.toStdString()
+		<< "::" << function.m_name.toStdString() << "(";
+	for (unsigned i = 0; i < function.m_parameters.size()-1; ++i)
+		out << function.m_parameters[i] << ", ";
+	out << function.m_parameters.back() << ")";
+	return out;
+}
+
+} // namespace end: codegen
--- moduleCodegen/memberfunction.hpp
+++ moduleCodegen/memberfunction.hpp
@@ -1,11 +1,25 @@
 #ifndef MEMBERFUNCTION_HPP
 #define MEMBERFUNCTION_HPP
 
+#include <QString>
+#include "type.hpp"
+#include "variable.hpp"
+#include "function.hpp"
 
-class MemberFunction
-{
+namespace codegen {
+
+class MemberFunction : public Function {
 public:
-	MemberFunction();
+	MemberFunction(Type returnType, QString functionName, const std::vector<Variable> &parameters, QString owner);
+
+	// TODO: Switch this to a reference to the owner class (later once I construct class)
+	QString getOwnerName() const;
+
+	friend std::ostream& operator<<(std::ostream& out, const MemberFunction &f);
+private:
+	QString m_owner;
 };
+
+}
 
 #endif // MEMBERFUNCTION_HPP
