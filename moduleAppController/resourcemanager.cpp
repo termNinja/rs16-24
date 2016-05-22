64cf6dbfced3b8e242762e4cc292808a8994acf6
termninja <nmicovic@outlook.com>
2016-05-22 21:39:42 +0200

    Created resource manager. Still needs a lot of work.

--- moduleAppController/resourcemanager.cpp
+++ moduleAppController/resourcemanager.cpp
@@ -1,6 +1,65 @@
+#include <boost/filesystem.hpp>
+#include <QDir>
+#include <QString>
+#include <iostream>
+#include <sstream>
 #include "resourcemanager.hpp"
+
+namespace app {
+
+// Not used
+std::string ResourceManager::m_projectOutputPath = "../../projectOutput";
+
+std::string ResourceManager::m_projectName = "dummyProjectShowcase";
+bool ResourceManager::m_alive = false;
+
+ResourceManager &ResourceManager::instance()
+{
+	static ResourceManager rm;
+
+	if (!m_alive) {
+		initResourceManager();
+		m_alive = true;
+	}
+
+	return rm;
+}
+
+std::string ResourceManager::getProjectOutputPath() const
+{
+	return m_projectOutputPath;
+}
+
+std::string ResourceManager::getProjectName() const
+{
+	return m_projectName;
+}
 
 ResourceManager::ResourceManager()
 {
-
 }
+
+void ResourceManager::initResourceManager()
+{
+	// =====-----
+	// TODO <----
+	// -----=====
+	// Once file dialog is added into application,
+	// this method needs to be connected to the folder
+	// user picks.
+	// For now, I shall use a dummy code that creates a dummy folder
+	// inside executables until that part of app is completed.
+	const QString path = QString::fromStdString(m_projectName);
+	QDir qdir;
+
+	std::cout << "Initializing resource manager..." << std::endl;
+	std::cout << "Attempting to create project folder -> " << m_projectName << std::endl;
+	if (qdir.mkdir(path))
+		std::cout << "Output folder created at " << path.toStdString() << std::endl;
+	else if (qdir.exists(path))
+		std::cout << "Output folder already exists!" << std::endl;
+	else
+		std::cout << "Failed creating output folder!  " << std::endl;
+}
+
+} // namespace end: app
--- moduleAppController/resourcemanager.hpp
+++ moduleAppController/resourcemanager.hpp
@@ -1,11 +1,35 @@
 #ifndef RESOURCEMANAGER_HPP
 #define RESOURCEMANAGER_HPP
 
+#include <string>
+#include <fstream>
+#include <boost/filesystem.hpp>
 
-class ResourceManager
-{
+namespace app {
+
+class ResourceManager {
 public:
+
+	static ResourceManager& instance();
+	std::string getProjectOutputPath() const;
+	std::string getProjectName() const;
+
+private:
 	ResourceManager();
+
+	ResourceManager(const ResourceManager &) = delete;
+	ResourceManager(const ResourceManager &&) = delete;
+	ResourceManager& operator=(const ResourceManager&) = delete;
+	ResourceManager& operator=(const ResourceManager&&) = delete;
+
+	static void initResourceManager();
+
+protected:
+	static std::string m_projectOutputPath;
+	static std::string m_projectName;
+	static bool m_alive;
 };
+
+} // namespace end: app
 
 #endif // RESOURCEMANAGER_HPP
